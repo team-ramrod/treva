1540e95db5df0c42faceb57ecea7ee0e176c545a
Wim Looman <ghostunderscore@gmail.com>
2011-10-04 11:43:24 +1300

    Got rid of some stupid optimisations

--- leros/asm/count.asm
+++ leros/asm/count.asm
@@ -7,28 +7,32 @@ start:
     store   r1
 
 loop:
+    load    0
     loadh   2
     store   r0
 
 ll1:
+    load    255
     loadh   255
 
 ll2:
     sub     1
+    nop
     brnz    ll2
-    load    255
+    nop
 
     load    r0
     sub     1
     store   r0
 
+    nop
     brnz    ll1
-    load    255
+    nop
 
     load    r1
     add     1
     out     0
     store   r1
 
+    nop
     branch  loop
-    load    0
