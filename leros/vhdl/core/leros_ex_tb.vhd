299d753a77a99abdd760a0485e76af9b355de831
Benjamin <benjwy@gmail.com>
2011-10-04 15:32:58 +1300

    Draft TB

--- leros/vhdl/core/leros_ex_tb.vhd
+++ leros/vhdl/core/leros_ex_tb.vhd
@@ -15,15 +15,15 @@ architecture TB of dff_TB is
                 clk : in std_logic;
                 reset : in std_logic;
                 din : in fedec_out_type;
-                ioin : in io_in_type;
+                ioin : in io_in_type; 
                 dout : out ex_out_type
     );
     end component;
     
-    signal op : std_logic_vector(1 downto 0); -- add, lsh, or etc
-    signal accu, opd  : unsigned(15 downto 0);
-    signal logic, arith, shift, a_mux : unsigned (15 downto 0);
-    signal op_class : op_class_type;
+    --signal op : std_logic_vector(1 downto 0); -- add, lsh, or etc
+    --signal accu, opd  : unsigned(15 downto 0);
+    --signal logic, arith, shift, a_mux : unsigned (15 downto 0);
+    --signal op_class : op_class_type;
     
     signal T_clk : std_logic;
     signal T_reset : std_logic;
@@ -34,17 +34,33 @@ architecture TB of dff_TB is
    
 begin
 
-    U_LEROS_EX: leros_ex port map (T_clk, T_reset, T_din, T_ioin, T_dout);
+    __LEROS_EX: leros_ex port map (T_clk, T_reset, T_din, T_ioin, T_dout);
     
+    T_reset <= '0';
+    
+    -- concurrent process to offer clock signal     
+    process
+    begin
+        T_clk <= '0';
+        wait for 5 ns;
+        T_clk <= '1';
+        wait for 5 ns;
+    end process;
     
     process
-
---         variable err_cnt: integer := 0; 
-
+        variable err_cnt: integer := 0; 
     begin
                 
-                
         -- case 1
+        --T_din.dec.op <= "00"; -- load
+        T_din.dec.op_class <= io_flag;
+        T_ioin <= "0000000000000000";
+        wait for 10 ns;
+        assert (T_dout.accu = "0000000000000000") report "Error" severity error;
+        if (T_dout.accu /= "0000000000000000") then
+            err_cnt := err_cnt + 1;
+        end if;
+        
     end process;
 
 end TB;
