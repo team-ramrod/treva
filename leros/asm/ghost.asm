f42c72bcf052d882a6a8fed13000231d8a2fb1a7
Henry Jenkins <henry@henryjenkins.name>
2011-10-06 23:03:44 +1300

    Finished code for ghosting
    This needs checking by a couple of people before it should be used

--- leros/asm/ghost.asm
+++ leros/asm/ghost.asm
@@ -1,3 +1,14 @@
+# r0 is used for passing data
+# r1 for passing address
+# r2 Image width
+# r3 Image height
+# r4 i v1
+# r5 i v2
+# r6 Number of images we have loaded into memory
+# r7 is image size
+# r8 is image1 pixel
+# r9 result = (im2 + im1)/2
+# r10 is address of image2 pixel
     nop
 
 # Setting up the address register for storing image data to ram
@@ -38,11 +49,10 @@ read_image:
 # size = temp
     store r7
 
-
 # read in bytes
+    store r4
 tits:
 # count = temp
-    store r4
 
 # byte = uart_read()
     load <read
@@ -62,7 +72,7 @@ tits:
 # temp = count -1
     load r4
     sub 1
-    nop
+    store r4
 # if(temp != 0) goto tits;
     brnz tits
 
@@ -121,8 +131,8 @@ ghost:
    jal r1
    nop
 
-# write out height (assuming same as width)
-   load r2
+# write out height
+   load r3
    store r0
    load <write
    loadh >write
@@ -131,7 +141,34 @@ ghost:
    nop
 
 
-# write out bytes
+# Write out image
+# Known stuff:
+#   r7 is image size
+#   image starts at 20 + 2 * r7
+# use r5 as i
+  load 0
+  loadh 0
+  store r5      # i = 0
+  load r7
+  add r7
+  store r8  # using r8 as 0th pixel of image
+image_write:
+  load r8
+  add r5        # i + image_start_address
+  store r9
+  load r9 +20
+  store r0      # Load pixel into r0 for write
+  load <write
+  loadh >write
+  jal r1
+
+  load r5       # Now increment i
+  add 1
+  store r5
+  sub r7        # Compare to num px
+  nop           # Then loop
+  brnz image_write
+  nop
 
 # Reads byte from uart then places it into r0 and jumps to r1
 read:
