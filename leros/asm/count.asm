a70746a6379aa5f6866722d53d4902f27a7b47a5
Wim Looman <ghostunderscore@gmail.com>
2011-10-07 10:56:51 +1300

    New vhdliser

--- leros/asm/count.asm
+++ leros/asm/count.asm
@@ -2,37 +2,37 @@
     nop
 
 start:
-    load    0
-    loadh   0
-    store   r1
+    load      0
+    loadh     0
+    store     r1
 
 loop:
-    load    128
+    load      128
     loadh     0
-    store   r0
+    store     r0
 
 ll1:
-    load    255
-    loadh   255
+    load      255
+    loadh     255
 
 ll2:
-    sub     1
+    sub       1
     nop
-    brnz    ll2
-    nop
-
-    load    r0
-    sub     1
-    store   r0
-
-    brnz    ll1
+    brnz      ll2
     nop
 
-    load    r1
-    add     1
-    out     0   1
-    store   r1
+    load      r0
+    sub       1
+    store     r0
 
-    load 1
+    brnz      ll1
     nop
-    brnz    loop
+
+    load      r1
+    add       1
+    out       0       1
+    store     r1
+
+    load      1
+    nop
+    brnz      loop
