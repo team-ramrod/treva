12d73a388e405ffa12bcaed4c8f4bdd733fb21a1
Wim Looman <ghostunderscore@gmail.com>
2011-10-07 02:28:12 +1300

    new hello

--- leros/asm/hello.asm
+++ leros/asm/hello.asm
@@ -10,6 +10,10 @@
 start:
 	load 76
 	store r0
+  load <pause
+  nop
+  jal r1
+  nop
 	load <send
 	nop
 	jal r1
@@ -17,6 +21,10 @@ start:
 
 	load 101
 	store r0
+  load <pause
+  nop
+  jal r1
+  nop
 	load <send
 	nop
 	jal r1
@@ -24,6 +32,10 @@ start:
 
 	load 114
 	store r0
+  load <pause
+  nop
+  jal r1
+  nop
 	load <send
 	nop
 	jal r1
@@ -31,6 +43,10 @@ start:
 
 	load 111
 	store r0
+  load <pause
+  nop
+  jal r1
+  nop
 	load <send
 	nop
 	jal r1
@@ -38,6 +54,10 @@ start:
 
 	load 115
 	store r0
+  load <pause
+  nop
+  jal r1
+  nop
 	load <send
 	nop
 	jal r1
@@ -45,6 +65,10 @@ start:
 
 	load 13
 	store r0
+  load <pause
+  nop
+  jal r1
+  nop
 	load <send
 	nop
 	jal r1
@@ -52,6 +76,10 @@ start:
 
 	load 10
 	store r0
+  load <pause
+  nop
+  jal r1
+  nop
 	load <send
 	nop
 	jal r1
@@ -61,11 +89,6 @@ end:
 	branch start
 
 send:
-	in 0 3	// check tdre
-	and 1
-	nop	// one delay slot
-	brz send
-  nop
 	load r0
   nop
 	out 0 4
@@ -74,3 +97,30 @@ send:
 	nop
 	jal r1	// here r1 is just dummy
   nop
+
+pause:
+    load    128
+    loadh     0
+    store   r0
+
+ll1:
+    load    255
+    loadh   255
+
+ll2:
+    sub     1
+    nop
+    brnz    ll2
+    nop
+
+    load    r0
+    sub     1
+    store   r0
+
+    brnz    ll1
+    nop
+
+    load r1
+    nop
+    jal r1
+    nop
